01fba351bbfa6f7fc1ee57601a4a85cbcf36d5e0
Caliton <30415229+Caliton@users.noreply.github.com>
2020-03-30 09:57:14 -0300

    Update SCRIPT_CREATE.sql

--- SCRIPT_CREATE.sql
+++ SCRIPT_CREATE.sql
@@ -5,9 +5,9 @@ CREATE SCHEMA IF NOT EXISTS `help_me_fest` DEFAULT CHARACTER SET latin1 ;
 USE `help_me_fest` ;
 
 -- -----------------------------------------------------
--- Table `help_me_fest`.`__efmigrationshistory`
+-- Table `__efmigrationshistory`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `help_me_fest`.`__efmigrationshistory` (
+CREATE TABLE IF NOT EXISTS `__efmigrationshistory` (
   `MigrationId` VARCHAR(95) NOT NULL,
   `ProductVersion` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`MigrationId`))
@@ -16,35 +16,35 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `help_me_fest`.`departament`
+-- Table `departament`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `help_me_fest`.`departament` (
+CREATE TABLE IF NOT EXISTS `departament` (
   `Id` INT(11) NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(30) CHARACTER SET 'utf8mb4' NOT NULL,
   PRIMARY KEY (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 2
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `help_me_fest`.`profile`
+-- Table `profile`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `help_me_fest`.`profile` (
+CREATE TABLE IF NOT EXISTS `profile` (
   `Id` INT(11) NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(25) CHARACTER SET 'utf8mb4' NOT NULL,
   PRIMARY KEY (`Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 2
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `help_me_fest`.`person`
+-- Table `person`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `help_me_fest`.`person` (
+CREATE TABLE IF NOT EXISTS `person` (
   `Id` INT(11) NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(25) CHARACTER SET 'utf8mb4' NOT NULL,
+  `Name` VARCHAR(50) CHARACTER SET 'utf8mb4' NOT NULL,
   `IsGuest` TINYINT(1) NOT NULL,
   `Discriminator` LONGTEXT CHARACTER SET 'utf8mb4' NOT NULL,
   `RelatedUserId` INT(11) NULL DEFAULT NULL,
@@ -54,28 +54,27 @@ CREATE TABLE IF NOT EXISTS `help_me_fest`.`person` (
   `ProfileId` INT(11) NULL DEFAULT NULL,
   `DepartamentId` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`Id`),
-
   CONSTRAINT `FK_Person_Departament_DepartamentId`
     FOREIGN KEY (`DepartamentId`)
-    REFERENCES `help_me_fest`.`departament` (`Id`)
+    REFERENCES `departament` (`Id`)
     ON DELETE CASCADE,
   CONSTRAINT `FK_Person_Person_RelatedUserId`
     FOREIGN KEY (`RelatedUserId`)
-    REFERENCES `help_me_fest`.`person` (`Id`)
+    REFERENCES `person` (`Id`)
     ON DELETE CASCADE,
   CONSTRAINT `FK_Person_Profile_ProfileId`
     FOREIGN KEY (`ProfileId`)
-    REFERENCES `help_me_fest`.`profile` (`Id`)
+    REFERENCES `profile` (`Id`)
     ON DELETE CASCADE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `help_me_fest`.`event`
+-- Table `event`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `help_me_fest`.`event` (
+CREATE TABLE IF NOT EXISTS `event` (
   `Id` INT(11) NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(50) CHARACTER SET 'utf8mb4' NOT NULL,
   `DateInitial` DATETIME(6) NOT NULL,
@@ -86,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `help_me_fest`.`event` (
   PRIMARY KEY (`Id`),
   CONSTRAINT `FK_Event_Person_EventOrganizerId`
     FOREIGN KEY (`EventOrganizerId`)
-    REFERENCES `help_me_fest`.`person` (`Id`)
+    REFERENCES `person` (`Id`)
     ON DELETE CASCADE)
 ENGINE = InnoDB
 AUTO_INCREMENT = 3
@@ -94,19 +93,19 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `help_me_fest`.`userevent`
+-- Table `userevent`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `help_me_fest`.`userevent` (
-  `IdUser` INT(11) NOT NULL,
-  `IdEvent` INT(11) NOT NULL,
-  `UserId` INT(11) NULL DEFAULT NULL,
-  `EvetId` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`IdUser`, `IdEvent`),
-  CONSTRAINT `FK_UserEvent_Event_EvetId`
-    FOREIGN KEY (`EvetId`)
-    REFERENCES `help_me_fest`.`event` (`Id`),
-  CONSTRAINT `FK_UserEvent_Person_UserId`
-    FOREIGN KEY (`UserId`)
-    REFERENCES `help_me_fest`.`person` (`Id`))
+CREATE TABLE IF NOT EXISTS `userevent` (
+  `PersonId` INT(11) NOT NULL,
+  `EventId` INT(11) NOT NULL,
+  PRIMARY KEY (`PersonId`, `EventId`),
+  CONSTRAINT `FK_UserEvent_Event_EventId`
+    FOREIGN KEY (`EventId`)
+    REFERENCES `event` (`Id`)
+    ON DELETE CASCADE,
+  CONSTRAINT `FK_UserEvent_Person_PersonId`
+    FOREIGN KEY (`PersonId`)
+    REFERENCES `person` (`Id`)
+    ON DELETE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
